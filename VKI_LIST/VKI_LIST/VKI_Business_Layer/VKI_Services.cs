ba52be76c21336b91cb28211b973ea2e18a9065d
halil.toprak <halil.toprak@HALILTOPRAK>
2023-01-12 14:18:23 +0300

    VKI Listeleme Ödevi

---
+++ VKI_LIST/VKI_LIST/VKI_Business_Layer/VKI_Services.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKI_Business_Layer
+{
+    public class VKI_Services
+    {
+        private static List<VKI> VKIList = new List<VKI>();
+
+       public static void SaveVKIList(VKI vKI)
+        {
+            VKIList.Add(vKI);
+        }
+        public static IReadOnlyCollection<VKI> GetVKIList() 
+        { 
+            return VKIList.AsReadOnly();
+        }
+
+    }
+}
---
+++ VKI_LIST/VKI_LIST/VKI_UserInterFace_Layer/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Runtime.InteropServices;
+using VKI_Business_Layer;
+namespace VKI_Application
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("----------VKI Uygulaması------");
+            Console.WriteLine("1-VKI Hesaplama\n2-Listeleme\n3-Arama\n4-Çıkış");
+            MenuSelection();
+        }
+
+        public static void MenuSelection()
+        {
+            Console.WriteLine("Seçim Yapınız(1-4)");
+            string secim=Console.ReadLine();
+            switch(secim)
+            {
+                case "1":NewVKIRec(); break;
+                case "2":DumpVKILists(); break;
+                case "3":VKISearch(); break;
+                case "4":Environment.Exit(0);break;
+                default:Console.WriteLine("Hatalı Seçim"); MenuSelection();break;
+            }
+        }
+
+        public static void NewVKIRec()
+        {
+            do
+            {
+                Console.Clear();
+                VKI vki = new VKI();
+                Console.Write("Kişi Adını Giriniz:");
+                vki.Ad = Console.ReadLine();
+                Console.Write("Kişi Soyadını Giriniz:");
+                vki.Soyad = Console.ReadLine();
+                Console.Write("Telefonuzu Giriniz:");
+                vki.TelNo = Console.ReadLine();
+                Console.Write("Boyunuzu Giriniz(cm):");
+                vki.Boy = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Klilonuzu Giriniz:");
+                vki.Kilo = Convert.ToDouble(Console.ReadLine());
+                VKI_Services.SaveVKIList(vki);
+                DumpToScreen(vki);
+            } while (Devamdurumu());
+            Menu();
+
+
+            
+        }
+        static bool Devamdurumu()
+
+        {
+            Console.Write("Devam mı(e/h)?'");
+            char devam = Console.ReadKey().KeyChar;
+
+            if (devam == 'e' || devam == 'E')
+            { return true; }
+            else if (devam == 'h' || devam == 'H')
+            { return false; }
+            else { Console.WriteLine("Hatalı Giriş Yaptınız Tekrar deneyin.."); return Devamdurumu(); }
+        }
+        private static void Again()
+        {
+            Console.WriteLine("Menüye dönmek için ENTER");
+            Console.ReadLine();
+            Menu();
+        }
+        public static void DumpVKILists()
+        {
+            var vki_list=VKI_Services.GetVKIList();
+            foreach(VKI vki in vki_list)
+            {
+                DumpToScreen(vki); ;
+            }
+            Again();
+        }
+        private static void VKISearch()
+        {
+            throw new NotImplementedException();
+        }
+        public static void DumpToScreen(VKI v)
+        {
+            Console.WriteLine($"Adı:{v.Ad} Soyadı:{v.Soyad} Telefonu:{v.TelNo} Boyu:{v.Boy} Kilosu:{v.Kilo} VKI:{v.VKI_Hesapla()} Teşhis:{v.VKI_Stat()}");
+        }
+
+    }
+}
