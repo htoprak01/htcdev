014d4d34c4e9950f5107cf038122dead8adb422f
halil.toprak <halil.toprak@HALILTOPRAK>
2023-01-14 19:35:16 +0300

    VKI file Ödevi Tamalandı

---
+++ VKI_LIST/VKI_LIST/Data_Layer/Filetransactions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Layer
+{
+    public class Filetransactions
+    {
+        private const string filename = "VKI_TextFile.txt";
+        public static void Filewrite(string Data)
+        {
+            File.WriteAllText(filename,Data);
+        }
+        public static string Fileread()
+        {
+            if(File.Exists(filename))        
+            return File.ReadAllText(filename);          
+            return string.Empty;         
+        }
+    }
+}
