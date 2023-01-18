cc6799ab02677ba0484fd090808afa2d2ee0e20c
halil.toprak <halil.toprak@HALILTOPRAK>
2023-01-18 14:10:15 +0300

    Arama-Silme güncellemesi

---
+++ VKI_Linq/VKI_Linq/VKI_Business_Layer/VKI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKI_Business_Layer
+{
+    public class VKI
+    {
+        public string Ad;
+        public string Soyad;
+        public string TelNo;
+        public double Boy;
+        public double Kilo;
+        public double VKI_Hesapla()
+        {
+            return Kilo/((Boy/100)*(Boy/100));
+        }
+        public string VKI_Stat()
+        {
+            double sonuc = VKI_Hesapla();
+            if (sonuc < 18.49) { return "ZAYIF"; }
+            else if (sonuc > 18.50 && sonuc < 24.99) { return "ZAYIF"; }
+            else if (sonuc> 18.50 && sonuc< 24.99) { return "IDEAL"; }
+            else if (sonuc > 25 && sonuc < 29.99) { return "HAFİF KİLOLU"; }
+            else if (sonuc > 30) { return "OBEZ"; }
+            else { return "DIĞRER"; }
+        }
+
+    }
+}
---
+++ VKI_Linq/VKI_Linq/VKI_Business_Layer/VKI_Services.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Data_Layer;
+namespace VKI_Business_Layer
+{
+    public class VKI_Services
+    {
+        private static List<VKI> VKIList = new List<VKI>();
+
+        public static void SaveVKIList(VKI vKI)
+        {
+            VKIList.Add(vKI);
+
+            //JSON serilaize
+            JsonSerializerOptions options = new JsonSerializerOptions();
+            options.IncludeFields = true;
+            string json = JsonSerializer.Serialize(VKIList, options);
+            // write to file
+            Filetransactions.Filewrite(json);
+
+        }
+        public static IReadOnlyCollection<VKI> GetVKIList()
+        {
+            string json = Filetransactions.Fileread();
+            VKIList = JsonSerializer.Deserialize<List<VKI>>(json, new JsonSerializerOptions { IncludeFields = true });
+
+
+            return VKIList.AsReadOnly();
+        }
+        public static IReadOnlyCollection<VKI> SearchByName(string name)
+        {
+            GetVKIList();
+            //var SearchRecord=new List<VKI>();
+            //foreach(var item in VKIList)
+            //{
+            //    if (item.Ad==name) SearchRecord.Add(item);
+            //}
+            //return SearchRecord.AsReadOnly();
+            var ResultRec = VKIList.Where(a => a.Ad == name).ToList();
+            return ResultRec;
+        }
+
+        public static void  RemoveRec(string name)
+        {
+            GetVKIList();
+            var ResultDeleteList = VKIList.Where(a => a.Ad!= name).ToList();
+            Filetransactions.DeleteFile();
+            foreach(VKI item in ResultDeleteList)
+            {
+                VKI_Services.SaveVKIList(item);
+            }
+           
+        }
+
+    }
+}
